7b23c13a9f36182d86c599cf525a404b5dbf75ac
Rebecca Lee <rlee291@aucklanduni.ac.nz>
2017-07-31 22:16:20 +1200

    forgot to add files

---
+++ Shared/Models/CanvasComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+using Windows.UI.Xaml.Shapes;
+
+namespace Shared.Models
+{
+    class CanvasComponent
+    {
+        public enum ComponentType { Ellipse, Polygon }
+
+        public ComponentType type { get; set; }
+        public Shape shape { get; set; }
+
+        // ellipse specific fields
+        public Point center { get; set; }
+        public double a { get; set; }
+        public double b { get; set; }
+        public double rotAngle { get; set; }
+
+        // polygon specific fields
+        public List<Point> points = new List<Point>();
+
+        public CanvasComponent(ComponentType type)
+        {
+            this.type = type;
+        }
+    }
+}
