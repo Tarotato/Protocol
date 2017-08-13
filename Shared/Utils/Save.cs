c986cefda3d07459bfdb9c0b6cc3b6096a10aa27
Isabel Zhuang <isabelz123@gmail.com>
2017-08-13 16:59:52 +1200

    Add saving with template functionality

--- Shared/Utils/Save.cs
+++ Shared/Utils/Save.cs
@@ -3,15 +3,21 @@ using Shared.Models;
 using Shared.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
+using Windows.Storage.FileProperties;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Shared.Utils
 {
@@ -23,11 +29,14 @@ namespace Shared.Utils
         public async void SaveAsImage(double width, double height, List<InkStrokeContainer> _strokes)
         {
             // Set up and launch the Save Picker
-            FileSavePicker fileSavePicker = new FileSavePicker();
-            fileSavePicker.FileTypeChoices.Add("JPEG", new string[] { ".jpeg" });
-            fileSavePicker.FileTypeChoices.Add("PNG", new string[] { ".png" });
+            //FileSavePicker fileSavePicker = new FileSavePicker();
+            //fileSavePicker.FileTypeChoices.Add("JPEG", new string[] { ".jpeg" });
+            //fileSavePicker.FileTypeChoices.Add("PNG", new string[] { ".png" });
+            //var InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation.CreateFolderAsync(@"Assets\ProfilePictures", CreationCollisionOption.OpenIfExists);
+            //StorageFile file = await fileSavePicker.PickSaveFileAsync();
+            StorageFolder pictureFolder = ApplicationData.Current.LocalFolder;
+            var file = await pictureFolder.CreateFileAsync("tempImage.jpeg", CreationCollisionOption.ReplaceExisting);
 
-            StorageFile file = await fileSavePicker.PickSaveFileAsync();
             if (file != null)
             {
                 // At this point, the app can begin writing to the provided save file
@@ -56,7 +65,62 @@ namespace Shared.Utils
                         ShowFlyoutAboveInkToolbar?.Invoke("Image Saved");
                     }
                 }
+            var bitmap = new WriteableBitmap(1, 1);
+            var savedfiles = await pictureFolder.GetFilesAsync();
+             BitmapImage bitmapImage = new BitmapImage();
+            WriteableBitmap w;
+            foreach (var imageFile in savedfiles)
+            {
+                if (imageFile.Name.StartsWith("tempImage"))
+                {
+                    using (IRandomAccessStream fileStream = await imageFile.OpenAsync(FileAccessMode.Read))
+                    {
+                        bitmapImage.DecodePixelHeight = 100;
+                        bitmapImage.DecodePixelWidth = 100;
+                        await bitmapImage.SetSourceAsync(fileStream);                        
+                    }
+
+                    int h = bitmapImage.PixelHeight;
+                    int wd = bitmapImage.PixelWidth;
+
+                    using (var stream = await file.OpenReadAsync())
+                    {
+                        w = new WriteableBitmap(wd, h);
+                        await w.SetSourceAsync(stream);
+                        //Other code
+                    thingAsync(w);
+                    }
+                }
             }
+            }
+
+        }
+
+        private async void thingAsync(WriteableBitmap wBitmap)
+        {
+            var writeableBmp = new WriteableBitmap(1, 1);
+
+            var image1 = wBitmap;
+            var image2 = await writeableBmp.FromContent(new Uri("ms-appx:///Assets/browser.png"));
+
+            image1.Blit(new Rect(0, 0, image1.PixelWidth, image1.PixelHeight), image2, new Rect(0, 0, image2.PixelWidth, image2.PixelHeight));
+
+            // Save the writeableBitmap object to JPG Image file 
+            FileSavePicker picker = new FileSavePicker();
+            picker.FileTypeChoices.Add("JPG File", new List<string>() { ".jpg" });
+            StorageFile savefile = await picker.PickSaveFileAsync();
+            if (savefile == null)
+                return;
+            IRandomAccessStream stream = await savefile.OpenAsync(FileAccessMode.ReadWrite);
+            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+            // Get pixels of the WriteableBitmap object 
+            Stream pixelStream = image1.PixelBuffer.AsStream();
+            byte[] pixels = new byte[pixelStream.Length];
+            await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+            // Save the image file with jpg extension 
+            encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)image1.PixelWidth, (uint)image1.PixelHeight, 96.0, 96.0, pixels);
+            await encoder.FlushAsync();
+
         }
 
         public async Task<StorageFolder> SaveProject(StorageFolder storageFolder, List<InkStrokeContainer> strokes, ProjectMetaData metaData)
