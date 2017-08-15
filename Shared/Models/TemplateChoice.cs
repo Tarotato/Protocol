5451a958d9b863be8ba771a3d4660e872c87c05b
Rebecca Lee <rlee291@aucklanduni.ac.nz>
2017-08-15 12:10:39 +1200

    forgot a class

---
+++ Shared/Models/TemplateChoice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public enum TemplateChoice
+    {
+        Browser, MobYX, MobXX, MobYY, None
+    }
+}
